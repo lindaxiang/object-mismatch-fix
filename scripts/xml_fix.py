3a0826241494b03c08bc0336488be30a1a4920c7
lindaxiang <linda.xiang@oicr.on.ca>
2018-10-12 10:52:48 -0400

    modify the code uniform for collab/aws process and rely on the list to distinguish

--- scripts/xml_fix.py
+++ scripts/xml_fix.py
@@ -12,7 +12,8 @@ from overture_song.client import Api, ManifestClient, StudyClient
 from overture_song.tools import FileUploadClient
 from overture_song.utils import setup_output_file_path
 
-def generate_metadata_xml(xml_info, out_dir):
+def generate_metadata_xml(xml_info, app_ctx):
+    out_dir = app_ctx['ega_xml_dir']
 
     bundle_id, ega_metadata_repo, project_code, output_file, ega_dataset_id, ega_analysis_id, ega_experiment_id, ega_run_id, ega_sample_id, ega_study_id  = xml_info.rstrip().split("\t")
 
@@ -35,7 +36,7 @@ def generate_metadata_xml(xml_info, out_dir):
 
     except Exception as e:
         click.echo('Error {}'.format(str(e)))
-        with open('non_pcawg_fix.log', 'a') as f:
+        with open(app_ctx['log'], 'a') as f:
             f.write('{0}::{1}: {2}\n'.format(project_code, bundle_id, str(e)))
 
     return
@@ -53,49 +54,46 @@ def get_md5(fname):
 @click.command()
 @click.option('--config', '-c', default='conf.yaml', type=click.File())
 @click.option('--access_token', '-t', default=os.environ.get('ACCESSTOKEN',None))
-def main(config, access_token):
+@click.option('--profile', '-p', default='collab')
+def main(config, access_token, profile):
     
     app_ctx = yaml.load(config)
-    # app_ctx = replace_values(app_ctx)
-
-    aws_song_file = app_ctx['aws']
-    aws_song = {}
-    with open(aws_song_file, 'r', newline='') as f:
-        for l in f:
-            projectCode, objectId, analysisId, fileName, songMd5, songFileSize = l.rstrip().split('\t')
-            aws_song[objectId] = songMd5
 
     ega_xml_info = app_ctx['ega_xml_info']
     ega_xml = {}
-    with open(ega_xml_info, 'r', newline='\n') as f:
+    with open(ega_xml_info, 'r', newline='') as f:
         for l in f:
             analysisId = l.split('\t')[0]
             ega_xml[analysisId] = l.rstrip()
 
 
-    if os.path.isfile('fix.log'):
-        os.remove('fix.log')
+    if os.path.isfile(app_ctx['log']):
+        os.remove(app_ctx['log'])
     if os.path.isfile(app_ctx['xml_mismatch']):
         os.remove(app_ctx['xml_mismatch'])
-    if not os.path.isdir(app_ctx['collab_xml_fix_dir']):
-        os.mkdir(app_ctx['collab_xml_fix_dir'])
-    if not os.path.isdir(app_ctx['aws_xml_fix_dir']):
-        os.mkdir(app_ctx['aws_xml_fix_dir'])
+    if not os.path.isdir(app_ctx['xml_fix_dir']):
+        os.mkdir(app_ctx['xml_fix_dir'])
 
-    collab_song_file = app_ctx['collab']
-    with open(collab_song_file, 'r', newline='') as f:
+    song_file = app_ctx[profile]
+    with open(song_file, 'r', newline='') as f:
         for l in f:
             projectCode, objectId, analysisId, fileName, songMd5 = l.rstrip().split('\t')
 
-            # only download if there is not local copy
+            # double check for safety reason
+            if profile == 'aws' and not projectCode in app_ctx['aws_approved']:
+                with open(app_ctx['log'], 'a') as f:
+                    f.write('{0}::{1} object {2}: is not allowed in AWS\n'.format(projectCode, analysisId, objectId))
+                continue
+
+            # only download if there is no local copy
             fpath = os.path.join(app_ctx['xml_dir'], fileName)
             if not os.path.isfile(fpath):
                 # download the xml from collab
-                subprocess.check_output(['score-client', '--profile', 'collab', 'download','--object-id', objectId, '--validate', 'false', '--force', '--output-dir', app_ctx['xml_dir']])
+                subprocess.check_output(['score-client', '--profile', profile, 'download','--object-id', objectId, '--validate', 'false', '--force', '--output-dir', app_ctx['xml_dir']])
 
-            # get file md5sum for the one downloaded from collab
-            collabMd5 = get_md5(fpath)
-            collabSize = os.path.getsize(fpath)
+            # get file md5sum for the one downloaded by score client
+            scoreMd5 = get_md5(fpath)
+            scoreSize = os.path.getsize(fpath)
 
 
             # handle the ega xml
@@ -103,7 +101,7 @@ def main(config, access_token):
                 # generate the xml from ega jobs
                 if not ega_xml.get(analysisId):
                     click.echo('{}::{}: the ega transfer job is missing'.format(projectCode, analysisId))
-                    with open('fix.log', 'a') as f:
+                    with open(app_ctx['log'], 'a') as f:
                         f.write(
                             '{0}::{1}: the ega transfer job is missing in the completed folder\n'.format(projectCode,
                                                                                                          analysisId))
@@ -113,35 +111,37 @@ def main(config, access_token):
 
                 fpath = os.path.join(app_ctx['ega_xml_dir'], fileName)
                 if not os.path.isfile(fpath):
-                    generate_metadata_xml(ega_xml[analysisId], app_ctx['ega_xml_dir'])
+                    generate_metadata_xml(ega_xml[analysisId], app_ctx)
 
             # get file md5sum for the one to upload
             fileMd5 = get_md5(fpath)
             fileSize = os.path.getsize(fpath)
 
-            if not fileMd5 == songMd5 or not fileMd5 == collabMd5:
+            if not fileMd5 == songMd5 or not fileMd5 == scoreMd5:
                 with open(app_ctx['xml_mismatch'], 'a') as f:
-                    f.write('\t'.join([projectCode, analysisId, collabMd5, songMd5, fileMd5]))
+                    f.write('\t'.join([projectCode, analysisId, scoreMd5, songMd5, fileMd5]))
                     f.write('\n')
 
             # skip the fix ones
-            fixpath = os.path.join(app_ctx['collab_xml_fix_dir'], fileName + '.fix')
+            fixpath = os.path.join(app_ctx['xml_fix_dir'], fileName + '.fix')
             if os.path.isfile(fixpath): continue
 
-            # upload to collab
-            subprocess.check_output(['score-client', '--profile', 'collab', 'upload', '--md5', fileMd5, '--file', fpath, '--object-id', objectId, '--force'])
+            # upload to storage
+            subprocess.check_output(['score-client', '--profile', profile, 'upload', '--md5', fileMd5, '--file', fpath, '--object-id', objectId, '--force'])
 
             # copy xml to open meta bucket
-            subprocess.check_output(['aws', '--endpoint-url', app_ctx['collab_endpoint_url'], '--profile', 'collab', 's3', 'cp', fpath, app_ctx['meta_bucket_url']+objectId])
+            if profile == 'collab':
+                subprocess.check_output(['aws', '--endpoint-url', app_ctx[profile+'_endpoint_url'], '--profile', profile, 's3', 'cp', fpath, app_ctx['meta_bucket_url']+objectId])
+            else:
+                subprocess.check_output(['aws', '--profile', 'amazon_pay', 's3', 'cp', fpath, app_ctx['meta_bucket_url'] + objectId])
 
-
-            # update the collab song
-            server_url = app_ctx['song']['collab']
+            # update the song
+            server_url = app_ctx['song'][profile]
             api_config = ApiConfig(server_url,projectCode,access_token)
             api = Api(api_config)
 
 
-	        # check whether the collab-song payload need to be updated
+	        # check whether the song payload need to be updated
             if not fileMd5 == songMd5:
             # update the file
                 fileUpdate = FileUpdateRequest()
@@ -155,58 +155,10 @@ def main(config, access_token):
                 try:
                     api.publish(analysisId)
                 except:
-                    with open('fix.log', 'a') as f:
+                    with open(app_ctx['log'], 'a') as f:
                         f.write('{0}::{1}: can not be published\n'.format(projectCode, analysisId))
 
             with open(fixpath, 'w') as w: w.write('')
-
-            # skip the fix ones
-            fixpath = os.path.join(app_ctx['aws_xml_fix_dir'], fileName + '.fix')
-            if os.path.isfile(fixpath): continue
-
-            # check whether the object is aws approved
-            if projectCode in app_ctx['aws_approved'] and objectId in aws_song:
-	            # upload to aws
-                subprocess.check_output(['score-client', '--profile', 'aws', 'upload', '--md5', fileMd5, '--file', fpath, '--object-id', objectId, '--force'])
-
-	            # copy xml to open meta bucket
-                subprocess.check_output(['aws', '--profile', 'amazon_pay', 's3', 'cp', fpath, app_ctx['meta_bucket_url']+objectId])
-
-	            # update the aws song
-                server_url = app_ctx['song']['aws']
-                api_config = ApiConfig(server_url,projectCode,access_token)
-                api = Api(api_config)
-
-
-		        # check whether the aws-song payload need to be updated
-                if not fileMd5 == aws_song[objectId]:
-	                # update the file
-                    fileUpdate = FileUpdateRequest()
-                    fileUpdate.fileSize = fileSize
-                    fileUpdate.fileMd5sum = fileMd5
-                    api.update_file(objectId, fileUpdate)
-
-
-	            # publish the analysis
-                if not api.get_analysis(analysisId).__dict__['analysisState'] == "PUBLISHED":
-                    try:
-                        api.publish(analysisId)
-                    except:
-                        with open('fix.log', 'a') as f:
-                            f.write('{0}::{1}: can not be published\n'.format(projectCode, analysisId))
-
-                with open(fixpath, 'w') as w: w.write('')
-
-            elif projectCode in app_ctx['aws_approved'] and (not objectId in aws_song):
-                with open('fix.log', 'a') as f:
-                    f.write('{0}::{1} object {2}: is missing from AWS\n'.format(projectCode, analysisId, objectId))
-
-            elif (not projectCode in app_ctx['aws_approved']) and objectId in aws_song:
-                with open('fix.log', 'a') as f:
-                    f.write('{0}::{1} object {2}: is not allowed in AWS\n'.format(projectCode, analysisId, objectId))
-            else:
-                pass
-
 
     return
 
